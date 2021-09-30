82a7ca83fc51728a5839e4ecdfef6c4d21834701
ChristopherB91 <87198039+ChristopherB91@users.noreply.github.com>
2021-09-30 14:53:17 -0400

    Java HW#1

---
+++ HwClass.java
@@ -0,0 +1,84 @@
+package FirstPackage;
+
+public class HwClass {
+	
+//	1.Write an if/else statement for the following requirements:
+//		1.If student gets 90 or higher: console log  A
+//		2.If students get 80 or above: console log B
+//		3.If students get 70 or above: console log C
+//		4.If students get 55 or above: console log D
+//		5.Any grade lower than 55 is F
+	
+//	public static void main(String[] args) {
+//		int grade = 65;
+//		if(grade >= 90){
+//		System.out.println("You got an A");
+//		}
+//		if(grade >= 80 && grade < 90){
+//			System.out.println("You got a B");
+//		}
+//		if(grade >= 70 && grade < 80){
+//			System.out.println("You got a C");
+//		}
+//		if(grade >= 55 && grade < 70){
+//			System.out.println("You got a D");
+//		}
+//	}
+//	----------------------------------------------------------------------------------------------------------------------------------------
+//	2.Using switch case, do the following:
+//		1.Store a number between 1-7
+//		2.If the number is 1, display a message "Monday", 2 for "Tuesday", 3 for "Wednesday" etc.
+//		3.Anything other than 1-7 would default to "Invalid Input"
+	
+//	public static void main(String[] args) {
+//		int day = 10;
+//		switch ( day ) {
+//		case 1:
+//			System.out.println("Monday");
+//			break;
+//		case 2:
+//			System.out.println("Tuesday");
+//			break;
+//		case 3:
+//			System.out.println("Wednesday");
+//			break;
+//		case 4:
+//			System.out.println("Thursday");
+//			break;
+//		case 5:
+//			System.out.println("Friday");
+//			break;
+//		case 6:
+//			System.out.println("Saturday");
+//			break;
+//		case 7:
+//			System.out.println("Sunday");
+//			break;
+//		default:
+//			System.out.println("Invalid Input");
+//		}
+//		
+//	}
+//	-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//	3.Store a number in a variable called num and write an if statement: 
+//		1.If num is odd, display "Cool"
+//		2.If num is even and between 2-5, display "Not Cool"
+//		3.If num is even and between 6-20, display "Cool"
+//		4.If num is even and greater than 20, display "Not Cool
+	
+//	public static void main(String[] args) {
+//		int num = 8;
+//		if(num%2 != 0){
+//		System.out.println("cool");
+//		}
+//		if(num%2 == 0 && num > 2 && num < 5){
+//			System.out.println("not cool");
+//		}
+//		if(num%2 == 0 && num > 6 && num < 20){
+//			System.out.println("cool");
+//		}
+//		if(num%2 == 0 && num > 20){
+//			System.out.println("not cool");
+//		}
+//	}
+}
