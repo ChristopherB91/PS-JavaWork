b4c6dc2d786378ed2ef1970fb0debb9fbc2464cc
ChristopherB91 <87198039+ChristopherB91@users.noreply.github.com>
2021-10-06 12:31:21 -0400

    Java HW

--- JavaHW#3/src/FirstPackage/Hw3.java
+++ JavaHW#3/src/FirstPackage/Hw3.java
@@ -3,32 +3,70 @@ package FirstPackage;
 import java.util.Scanner;
 
 public class Hw3 {
-
-	public static void main(String[] args) {
+	static void Add() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Give me a number");
-	      int num1 = scan.nextInt();
+	    int add1 = scan.nextInt();
 	    System.out.println("Give me another number");
-	      int num2 = scan.nextInt();
+	    int add2 = scan.nextInt();
+	    int adda;
+	    adda = add1 + add2;
+	    System.out.println(adda);
+	    scan.close();
+	}
+	
+	static void Subtract() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Give me the biggger number first");
+	    int Sub1 = scan.nextInt();
+	    System.out.println("Give me another number");
+	    int Sub2 = scan.nextInt();
+	    int Suba;
+	    Suba = Sub1 - Sub2;
+	    System.out.println(Suba);
+	    scan.close();
+	}
+	
+	static void Multiply() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Give me a number");
+	    int Mul1 = scan.nextInt();
+	    System.out.println("Give me another number");
+	    int Mul2 = scan.nextInt();
+	    int Mula;
+	    Mula = Mul1 * Mul2;
+	    System.out.println(Mula);
+	    scan.close();
+	}
+	
+	static void Divide() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Give me the bigger number");
+	    int Div1 = scan.nextInt();
+	    System.out.println("Give me another number");
+	    int Div2 = scan.nextInt();
+	    int Diva;
+	    Diva = Div1 / Div2;
+	    System.out.println(Diva);
+	    scan.close();
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 	    System.out.println("1.Add 2.Subtract. 3.Multiply. 4.Divide. 5.Exit");
 	      int num3 = scan.nextInt();
-	      int ans;
 	      switch ( num3 ) {
 			case 1:
-				ans = num1 + num2;
-				System.out.println(ans);
+				Add();
 				break;
 			case 2:
-				ans = num1 - num2;
-				System.out.println(ans);
+				Subtract();
 				break;
 			case 3:
-				ans = num1 * num2;
-				System.out.println(ans);
+				Multiply();
 				break;
 			case 4:
-				ans = num1 / num2;
-				System.out.println(ans);
+				Divide();
 				break;
 			case 5:
 				System.out.println("Good Bye");
@@ -37,5 +75,6 @@ public class Hw3 {
 				System.out.println("Invalid Entry, Try Again");
 				break;
 	      }
+	   scan.close();
 	}
 }
